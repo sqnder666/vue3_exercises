798524ac81d6b988ddbb3514f2adc1d0d1cb62f0
sqnder666 <i.irudovi21@gmail.com>
2023-03-01 20:13:54 +0300

    70

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import Employee from './components/Employee.vue'
+import EmployeeForm from './components/EmployeeForm.vue'
 export default {
   data() {
     return {
@@ -23,16 +24,13 @@ export default {
     }
   },
   components: {
-    Employee
+    Developer, EmployeeForm
   },
   methods: {
-    change(id, name, surn){
-      this.developers = this.developers.map((developer) => {
-        if(developer.id === id){
-          developer.name = name;
-          developer.surn = surn;
-        }
-        return developer;
+    add(name, surn){
+      let id = this.developers.length + 1;
+      this.developers.push({
+        id, name, surn
       });
     }
   }
@@ -40,12 +38,7 @@ export default {
 </script>
 
 <template>
-<Employee v-for   ="developer in developers"
-		:id     ="developer.id"
-		:name   ="developer.name"
-		:surn   ="developer.surn"
-		:key    ="developer.id"
-    @change="change"/>
+<EmployeeForm @add="add"/>
 </template>
 
 <style>
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    data() {
+        return {
+            newName: ' ',
+            newSurn: ' ',
+        }
+    },
+    methods: {
+        save(){
+            this.$emit('add', this.newName, this.newSurn)
+        }
+    }
+}
+</script>
+
+<template>
+    <input class="button" v-model="newName">
+    <br>
+    <br>
+    <input class="button" v-model="newSurn">
+    <br>
+    <button class="button" @click="save">save</button>
+</template>
+<style>
+.button {
+    background-color: #293133; 
+    border-radius: 8px;
+    border: none;
+    color: rgb(255, 255, 255);
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 15px;
+    margin: 4px 2px;
+    cursor: pointer;
+    -webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+}
+</style>
